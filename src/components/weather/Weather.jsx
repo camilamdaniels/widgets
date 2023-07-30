fce129ee745e72606f2c864e710e19eda072f682
Camila Daniels <camiladaniels@Camilas-MacBook-Pro.local>
2023-07-30 11:36:23 -0500

    Debugged weather component.

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,4 +1,4 @@
-import { useTheme } from '@mui/material'
+import { useTheme, Typography } from '@mui/material'
 import React, { useState, useEffect } from 'react'
 import ReactWeather, { useOpenWeather } from 'react-open-weather'
 
@@ -10,27 +10,33 @@ const Weather = () => {
     const [geodata, setGeodata] = useState([]);
 
     useEffect(() => {
-      const fetchData = async () => {
+      const getUserCoordinates = () => {
         navigator.geolocation.getCurrentPosition(function(position) {
           setLat(position.coords.latitude);
           setLong(position.coords.longitude);
         });
-
-        await fetch(`${process.env.REACT_APP_API_URL}/reverse?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}`)
-        .then(res => res.json())
-        .then(result => {
-          setGeodata(result)
-          console.log(result);
-        });
       }
-      fetchData();
-    }, [latitude,longitude])
+  
+      getUserCoordinates()
+  
+      console.log(latitude)
+      console.log(longitude)
 
-    console.log(latitude)
-    console.log(longitude)
+      // const fetchData = async () => {
+      //   await fetch(`${process.env.REACT_APP_API_URL}/reverse?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}`)
+      //   .then(result => {
+      //     setGeodata(result)
+      //     // console.log(result);
+      //   });
+      // }
+      // fetchData();
+    }, [latitude, longitude])
+
+    // console.log(latitude)
+    // console.log(longitude)
     console.log(process.env.REACT_APP_API_KEY)
-    console.log(geodata[0].name)
-    
+    // console.log(geodata)
+
     const { data, isLoading, errorMessage } = useOpenWeather({
         key: process.env.REACT_APP_API_KEY,
         lat: latitude ? latitude : '48.137154',
@@ -49,16 +55,26 @@ const Weather = () => {
         borderRadius: '3rem'
     }
   return (
-    <ReactWeather 
-        isLoading={isLoading}
-        errorMessage={errorMessage}
-        data={data}
-        lang='en'
-        locationLabel={geodata ? geodata[0].name : 'Munich'}
-        unitsLabels={{ temperature: 'F', windSpeed: 'mph'}}
-        showForecast
-        theme={customStyles}
-    />
+    <>
+      {(!isLoading) ? (
+        <>
+          <ReactWeather 
+            isLoading={isLoading}
+            errorMessage={errorMessage}
+            data={data}
+            lang='en'
+            locationLabel=''
+            unitsLabels={{ temperature: 'F', windSpeed: 'mph'}}
+            showForecast
+            theme={customStyles}
+          />
+        </>
+      ) : (
+        // <Typography variant='h1'>Loading...</Typography>
+        <></>
+      )}
+    </>
+    
   )
 }
 
