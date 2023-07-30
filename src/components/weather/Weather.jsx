faf19f6642f6b7dd3bf1f0e31e1fd0b012737785
Camila Daniels <camiladaniels@Camilas-MacBook-Pro.local>
2023-07-30 10:14:56 -0500

    Updated weather component to use geolocation data to display local results. If location services are turned off, weather conditions for Munich will be displayed.

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,22 +1,51 @@
-import React from 'react'
+import { useTheme } from '@mui/material'
+import React, { useState, useEffect } from 'react'
 import ReactWeather, { useOpenWeather } from 'react-open-weather'
 
 const Weather = () => {
+    const { palette } = useTheme()
+    
+    const [latitude, setLat] = useState([]);
+    const [longitude, setLong] = useState([]);
+    const [geodata, setGeodata] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          setLat(position.coords.latitude);
+          setLong(position.coords.longitude);
+        });
+
+        await fetch(`${process.env.REACT_APP_API_URL}/reverse?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          setGeodata(result)
+          console.log(result);
+        });
+      }
+      fetchData();
+    }, [latitude,longitude])
+
+    console.log(latitude)
+    console.log(longitude)
+    console.log(process.env.REACT_APP_API_KEY)
+    console.log(geodata[0].name)
+    
     const { data, isLoading, errorMessage } = useOpenWeather({
-        key: '6533e6634dc61ad17ee40480603e25e7',
-        lat: '48.137154',
-        lon: '11.576124',
+        key: process.env.REACT_APP_API_KEY,
+        lat: latitude ? latitude : '48.137154',
+        lon: longitude ? longitude: '11.576124',
         lang: 'en',
-        unit: 'metric' // change to imperial for US
+        unit: 'imperial'
     })
 
     const customStyles = {
         fontFamily: 'Inter, sans-serif',
-        gradientStart: '#66d6a2',
-        gradientMid: '#66d6a2',
-        gradientEnd: '#a8edcb',
-        forecastIconColor: '#fff',
-        todayIconColor: '#fff',
+        gradientStart: palette.primary[300],
+        gradientMid: palette.primary[300],
+        gradientEnd: palette.primary[100],
+        forecastIconColor: palette.grey[100],
+        todayIconColor: palette.grey[100],
         borderRadius: '3rem'
     }
   return (
@@ -25,8 +54,8 @@ const Weather = () => {
         errorMessage={errorMessage}
         data={data}
         lang='en'
-        locationLabel='Munich'
-        unitsLabels={{ temperature: 'C', windSpeed: 'Km/h'}}
+        locationLabel={geodata ? geodata[0].name : 'Munich'}
+        unitsLabels={{ temperature: 'F', windSpeed: 'mph'}}
         showForecast
         theme={customStyles}
     />
