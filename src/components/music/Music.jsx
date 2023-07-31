1130465ffeb3dd542b7ef110836a86d3d485dca1
Camila Daniels <camiladaniels@Camilas-MacBook-Pro.local>
2023-07-30 21:16:59 -0500

    Updated music component so that it plays an audio file.

--- src/components/music/Music.jsx
+++ src/components/music/Music.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { 
     useTheme,
     Box,
@@ -11,38 +11,49 @@ import {
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious'
 import PlayArrowIcon from '@mui/icons-material/PlayArrow'
 import SkipNextIcon from '@mui/icons-material/SkipNext'
+import PauseIcon from '@mui/icons-material/Pause'
+import Sound from 'react-sound'
+import Song from './Song.mp3'
 
-const Music = () => {
+const Music = ( ) => {
     const theme = useTheme()
+    const [isPlaying, setIsPlaying] = useState(false)
 
     return (
         <Card sx={{ display: 'flex' }}>
             <Box sx={{ display: 'flex', flexDirection: 'column', backgroundColor: '#66d6a2' }}>
                 <CardContent sx={{ flex: '1 0 auto'}}>
                     <Typography component='div' variant='h5'>
-                        Live From Space 
+                        Tokyo Cafe 
                     </Typography>
                     <Typography color='#fff' component='div' fontSize='20px'>
-                        Mac Miller
+                        TVARI
                     </Typography>
                 </CardContent>
                 <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 2 }}>
                     <IconButton aria-label='previous'>
                         {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
                     </IconButton>
-                    <IconButton aria-label='play/pause'>
-                        <PlayArrowIcon sx={{ height: 38, width: 38}}/>
+                    <IconButton aria-label='play/pause' onClick={() => setIsPlaying(!isPlaying)}>
+                        {!isPlaying ? <PlayArrowIcon sx={{ height: 38, width: 38}}/> : <PauseIcon sx={{ height: 38, width: 38}}/>}
                     </IconButton>
                     <IconButton aria-label='next'>
                         {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
                     </IconButton>
                 </Box>
             </Box>
+            <Sound 
+                url={Song}
+                playStatus={
+                    isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED
+                }
+                playFromPosition={300}
+            />
             <CardMedia 
                 component='img'
                 sx={{ width: 151 }}
                 image={require('./album-cover.jpg')}
-                alt='Live From Space Cover Art'
+                alt='Tokyo Cafe Cover Art'
             />
         </Card>
     )
